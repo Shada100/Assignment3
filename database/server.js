c21255c6c785252da4538c0907268c6eb9823811
Shada Funmi <shadafunmi421@gmail.com>
2024-12-21 17:17:45 +0100

    done

--- database/server.js
+++ database/server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require("express-session");
-const cors = require('cors');
+const cors = require("cors");
 const taskRoutes = require("../routes/task"); // Task routes
 const userRoutes = require("../routes/user"); // User routes
 
 // Constants
 const PORT = 5501;
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/todoapp";
+const MONGO_URI =
+  "mongodb+srv://shadafunmi421:P6iCaCfKNOxkZ7v4@apicluster.z36wa.mongodb.net/?retryWrites=true&w=majority&appName=apicluster";
 
 const app = express();
 
@@ -22,7 +23,6 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log(`Connected and Server running on http://localhost:${PORT}`);
 });
-
 
 app.use(cors());
 
